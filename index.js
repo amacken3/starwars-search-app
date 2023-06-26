310c7fa322353a07968880242a66e5314669c0ad
amacken3 <aengus.2000@gmail.com>
2023-06-26 11:04:14 -0400

    added the ability for the search button to be used and it will retrieve data

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-//fetch('https://swapi.dev/api/films/1/')
- // .then(response => response.json())
- // .then(data => {
-    // Handle the retrieved data here
- //   console.log(data);
-//  })
-//  .catch(error => {
-    // Handle any errors that occur during the request
-  //  console.error(error);
-  //});
+let allFilms = [];
   fetch('https://swapi.dev/api/films/')
   .then(response => response.json())
   .then(data => {
@@ -17,14 +8,13 @@
       // Access film details like title, director, etc.
       console.log(film.title);
       console.log(film.director);
-      // ...
     });
   })
   .catch(error => {
     // Handle any errors that occur during the request
     console.error(error);
   });
-
+let allCharacters = [];
 // Fetch all characters
 fetch('https://swapi.dev/api/people/')
   .then(response => response.json())
@@ -42,6 +32,48 @@ fetch('https://swapi.dev/api/people/')
     // Handle any errors that occur during the request
     console.error(error);
   });
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+    // Add event listner to search button
+searchButton.addEventListener('click', performSearch);
+    // Search function
+function performSearch() {
+    const searchQueary = searchInput.value.toLowerCase();
+}
+    // filter serach
+const filteredFilms = allFilms.filter(film => {
+    return film.title.toLowerCase().includes(searchQuery);
+});
+
+displayFilms(filteredFilms);
+
+const filteredCharacters = allCharacters.filter(character => {
+    return character.name.toLowerCase().includes(searchQuery);
+  });
+
+  displayCharacters(filteredCharacters);
+  
+function displayFilms(films) {
+const filmListElement = document.getElementById('film-list');
+filmListElement.innerHTML = '';
+
+films.forEach(film => {
+    const filmItem = document.createElement('li');
+    filmItem.textContent = film.title;
+    filmListElement.appendChild(filmItem);
+  });
+}
+function displayCharacters(characters) {
+    const characterListElement = document.getElementById('character-list');
+    characterListElement.innerHTML = '';
+  
+    characters.forEach(character => {
+      const characterItem = document.createElement('li');
+      characterItem.textContent = character.name;
+      characterListElement.appendChild(characterItem);
+    });
+  }
+  
 // Handle the retrieved films data here
 const films = data.results;
 const filmListElement = document.getElementById('film-list');
